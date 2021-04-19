3ae5021ecdad00d71e61702394ad761cb75d4f4d
sihan sun <haniyya2@gmail.com>
2021-04-18 19:56:12 -0700

    complete

--- src/component/Form.js
+++ src/component/Form.js
@@ -2,12 +2,36 @@ import React ,{useState, useEffect}from "react";
 import * as yup from "yup"
 import axios from "axios"
 
+const formSchema = yup.object().shape({
+    name: yup.string()
+        .required("Name is a required ")
+        .min(3," Name must be 3 chars or longer"),
+
+    size: yup.string().oneOf(["small", "medium", "large"], "Size is require"), 
+
+    pepperoni: yup.boolean().oneOf([true, false]),
+    sausage: yup.boolean().oneOf([true, false]),
+    canadianBacon: yup.boolean().oneOf([true, false]),
+    spicyItalianSausage: yup.boolean().oneOf([true, false]),
+    grilledChicken: yup.boolean().oneOf([true, false]),
+    onions: yup.boolean().oneOf([true, false]),
+    greenPepper: yup.boolean().oneOf([true, false]),
+    dicedTomatos: yup.boolean().oneOf([true, false]),
+    blackOlives: yup.boolean().oneOf([true, false]),
+    roastedGarlic: yup.boolean().oneOf([true, false]),
+    artichokeHearts: yup.boolean().oneOf([true, false]),
+    threeCheese: yup.boolean().oneOf([true, false]),
+    pineapple: yup.boolean().oneOf([true, false]),
+    extraCheese: yup.boolean().oneOf([true, false]),
+
+    specialInstruction: yup.string()
+})
+
 
 export default function Form(){
     const initialFormState ={
         name:"",
         size:"",
-        topping:"",
         specialInstruction:"",
         pepperoni: false,
         sausage: false,
@@ -28,33 +52,41 @@ export default function Form(){
     const [pizza, setPizza] = useState([]);
     const [serverError, setServerError] = useState("")
     const[formState, setFormState] = useState(initialFormState)
-    const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true)
     const [errors, setErrors] = useState(initialFormState)
 
-    const formSchema = yup.object().shape({
-        name: yup.string()
-            .required("Name is a required ")
-            .min(3," Name must be 3 chars or longer"),
+    // const formSchema = yup.object().shape({
+    //     name: yup.string()
+    //         .required("Name is a required ")
+    //         .min(3," Name must be 3 chars or longer"),
 
-        size: yup.string().oneOf(["small", "medium", "large"], "Size is require"), 
+    //     size: yup.string().oneOf(["small", "medium", "large"], "Size is require"), 
 
-        pepperoni: yup.boolean(),
-        sausage: yup.boolean(),
-        canadianBacon: yup.boolean(),
-        spicyItalianSausage: yup.boolean(),
-        grilledChicken: yup.boolean(),
-        onions: yup.boolean(),
-        greenPepper: yup.boolean(),
-        dicedTomatos: yup.boolean(),
-        blackOlives: yup.boolean(),
-        roastedGarlic: yup.boolean(),
-        artichokeHearts: yup.boolean(),
-        threeCheese: yup.boolean(),
-        pineapple: yup.boolean(),
-        extraCheese: yup.boolean(),
+    //     pepperoni: yup.boolean(),
+    //     sausage: yup.boolean(),
+    //     canadianBacon: yup.boolean(),
+    //     spicyItalianSausage: yup.boolean(),
+    //     grilledChicken: yup.boolean(),
+    //     onions: yup.boolean(),
+    //     greenPepper: yup.boolean(),
+    //     dicedTomatos: yup.boolean(),
+    //     blackOlives: yup.boolean(),
+    //     roastedGarlic: yup.boolean(),
+    //     artichokeHearts: yup.boolean(),
+    //     threeCheese: yup.boolean(),
+    //     pineapple: yup.boolean(),
+    //     extraCheese: yup.boolean(),
 
-        specialInstruction: yup.string()
-    })
+    //     specialInstruction: yup.string()
+    // })
+
+    useEffect(() => {
+        formSchema.isValid(formState).then(valid => {
+            console.log("valid?", valid);
+            setIsButtonDisabled(!valid)
+            console.log('is buttont disabled:  ',isButtonDisabled)
+        })
+    },[formState])
 
     const validateChange = e => {
         yup
@@ -68,22 +100,15 @@ export default function Form(){
             })
     }
 
-    useEffect(() => {
-        formSchema.isValid(formState).then(valid => {
-            console.log("valid?", valid);
-            setIsButtonDisabled(!valid)
-        })
-    },[formState])
-
+  
     const postNewPizza = newPizza => {
         axios
-        .post("h",formState)
+        .post("h",newPizza)
         .then(Response => {
             setPizza(Response.data)
             setFormState({
                 name:"",
                 size:"",
-                topping:"",
                 specialInstruction:"",
             })
             setServerError(null)
@@ -93,35 +118,36 @@ export default function Form(){
         })
     }
 
+
     const formSubmit = e => {
         e.preventDefault()
         const newPizza ={
             name:formState.name.trim(),
             size:formState.size.trim(),
             specialInstruction:formState.specialInstruction.trim(),
-            topping:['pepperoni','sausage','canadianBacon', 'spicyItalianSausage',
-                    'grilledChicken', 'onions', 'greenPepper', 'dicedTomatos',
-                    'blackOlives', 'roastedGarlic', 'artichokeHearts', 'threeCheese',
-                    'pineapple','extraCheese'].filter(top => formState[top])
+            
         }
         postNewPizza(newPizza)
     }
 
     const inputChange = e => {
         console.log("change: value= ",e.target.value)
+    
         const {name, value, type, checked} = e.target
         const valueToUse = type ==="checkbox"? checked: value
         e.persist()
         const newFormData = {
             ...formState,
-            [e.target.name]:e.target.value ==="checkbox" ? e.target.checked : e.target.value
+            [e.target.name]:e.target.type ==="checkbox" ? e.target.checked : e.target.value
         }
         validateChange(e)
         setFormState(newFormData)
+        console.log("current state: ", formState)
     }
 
     return (
-        <form>
+        <form onSubmit = {formSubmit}>
+            
              {serverError ? <p className="error">{serverError}</p> : null}
             <h2>Build your own pizza</h2>
             <img src ="https://www.budgetbytes.com/wp-content/uploads/2010/07/Classic-Homemade-Pizza-Dough-close.jpg"/>
@@ -153,71 +179,72 @@ export default function Form(){
 
                 <label>Pepperoni
                     <input type="checkbox" name="pepperoni"
-                    checked = {pizza.pepperoni} onChange = {inputChange}/>
+                    checked = {formState.pepperoni} onChange = {inputChange}/>
+                    
                 </label>
 
                 <label>Sausage
                     <input type="checkbox" name="sausage"
-                    checked = {pizza.sausage} onChange = {inputChange}/>
+                    checked = {formState.sausage} onChange = {inputChange}/>
                 </label>
 
                 <label>Canadian Bacon
                     <input type="checkbox" name="canadianBacon"
-                    checked = {pizza.canadianBacon} onChange = {inputChange}/>
+                    checked = {formState.canadianBacon} onChange = {inputChange}/>
                 </label>
 
                 <label>Spicy Italian Sausage
                     <input type="checkbox" name="spicyItalianSausage"
-                    checked = {pizza.spicyItalianSausage} onChange = {inputChange}/>
+                    checked = {formState.spicyItalianSausage} onChange = {inputChange}/>
                 </label>
                 <label>Grilled Chicken
                     <input type="checkbox" name="grilledChicken"
-                    checked = {pizza.grilledChicken} onChange = {inputChange}/>
+                    checked = {formState.grilledChicken} onChange = {inputChange}/>
                 </label>
 
                 <label>Onions
                     <input type="checkbox" name="onions"
-                    checked = {pizza.onions} onChange = {inputChange}/>
+                    checked = {formState.onions} onChange = {inputChange}/>
                 </label>
 
                 <label>Green Pepper
                     <input type="checkbox" name="greenPepper"
-                    checked = {pizza.greenPepper} onChange = {inputChange}/>
+                    checked = {formState.greenPepper} onChange = {inputChange}/>
                 </label>
 
                 <label>Diced Tomatos
                     <input type="checkbox" name="dicedTomatos"
-                    checked = {pizza.dicedTomatos} onChange = {inputChange}/>
+                    checked = {formState.dicedTomatos} onChange = {inputChange}/>
                 </label>
 
                 <label>Black Olives
                     <input type="checkbox" name="blackOlives"
-                    checked = {pizza.blackOlives} onChange = {inputChange}/>
+                    checked = {formState.blackOlives} onChange = {inputChange}/>
                 </label>
 
                 <label>Roasted Garlic
                     <input type="checkbox" name="roastedGarlic"
-                    checked = {pizza.roastedGarlic} onChange = {inputChange}/>
+                    checked = {formState.roastedGarlic} onChange = {inputChange}/>
                 </label>
 
                 <label>Artichoke Heart
                     <input type="checkbox" name="artichokeHearts"
-                    checked = {pizza.artichokeHearts} onChange = {inputChange}/>
+                    checked = {formState.artichokeHearts} onChange = {inputChange}/>
                 </label>
 
                 <label>Three Cheese
                     <input type="checkbox" name="threeCheese"
-                    checked = {pizza.threeCheese} onChange = {inputChange}/>
+                    checked = {formState.threeCheese} onChange = {inputChange}/>
                 </label>
 
                 <label>Pineapple
                     <input type="checkbox" name="pineapple"
-                    checked = {pizza.pineapple} onChange = {inputChange}/>
+                    checked = {formState.pineapple} onChange = {inputChange}/>
                 </label>
 
                 <label>Extra Cheese
                     <input type="checkbox" name="extraCheese"
-                    checked = {pizza.extraCheese} onChange = {inputChange}/>
+                    checked = {formState.extraCheese} onChange = {inputChange}/>
                 </label>
 
             </div>
